80db862f9d0d94768f271271c2cc7dc2a39fa674
rvdnieuwendijk <robert@van-den-nieuwendijk.nl>
2018-01-10 13:45:18 +0100

    Update 121 BusSharingMode - Physical and Virtual.ps1
    Modified the loop variable $vm name into $CurrentVM because $VM is a constant variable defined in plugin 00 Connection Plugin for vcenter.ps1. Using $vm as the loop variable breaks plugin 200 VMs on ephemeral portgroup.ps1.

--- Plugins/60 VM/121 BusSharingMode - Physical and Virtual.ps1
+++ Plugins/60 VM/121 BusSharingMode - Physical and Virtual.ps1
@@ -3,17 +3,17 @@ $Header = "BusSharingMode - Physical and Virtual: [count]"
 $Comments = "The following VMs have physical and/or virtual bus sharing. A problem will occur in case of svMotion without reconfiguration of the applications which are using these virtual disks and also change of the VM configuration concerned."
 $Display = "Table"
 $Author = "Petar Enchev, Luc Dekens"
-$PluginVersion = 1.0
+$PluginVersion = 1.1
 $PluginCategory = "vSphere"
 
 # Start of Settings
 # End of Settings
 
 # BusSharingMode - Physical and Virtual
-ForEach ($vm in $FullVM){
-    $scsi = $vm.Config.Hardware.Device | Where-Object {$_ -is [VMware.Vim.VirtualSCSIController] -and ($_.SharedBus -eq "physicalSharing" -or $_.SharedBus -eq "virtualSharing")}
+ForEach ($CurrentVM in $FullVM){
+    $scsi = $CurrentVM.Config.Hardware.Device | Where-Object {$_ -is [VMware.Vim.VirtualSCSIController] -and ($_.SharedBus -eq "physicalSharing" -or $_.SharedBus -eq "virtualSharing")}
     if ($scsi){
-        $scsi | Select-Object @{N="VM";E={$vm.Name}},
+        $scsi | Select-Object @{N="VM";E={$CurrentVM.Name}},
             @{N="Controller";E={$_.DeviceInfo.Label}},
             @{N="BusSharingMode";E={$_.SharedBus}}
     }
@@ -21,3 +21,4 @@ ForEach ($vm in $FullVM){
 
 # Changelog
 ## 1.0 : Initial Version
+## 1.1 : Modified the loop variable $vm into $CurrentVM because $VM is a constant variable defined in plugin 00 Connection Plugin for vcenter.ps1.
