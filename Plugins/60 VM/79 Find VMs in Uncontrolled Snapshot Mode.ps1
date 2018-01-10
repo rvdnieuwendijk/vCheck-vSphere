a3f3bc2f3bbbff2f359a2dd1be22feb93da550bb
rvdnieuwendijk <robert@van-den-nieuwendijk.nl>
2018-01-10 13:52:41 +0100

    Update 79 Find VMs in Uncontrolled Snapshot Mode.ps1
    Added the -ErrorAction SilentlyContinue parameter to the Get-ChildItem calls to prevent errors for files that no longer exist. These errors were caused by snapshots created during backups where the snapshots were removed before the Get-ChildItem cmdlet was called.

--- Plugins/60 VM/79 Find VMs in Uncontrolled Snapshot Mode.ps1
+++ Plugins/60 VM/79 Find VMs in Uncontrolled Snapshot Mode.ps1
@@ -15,7 +15,7 @@ $Header = "VMs in uncontrolled snapshot mode: [count]"
 $Comments = "The following VMs are in snapshot mode, but vCenter isn't aware of it. See http://kb.vmware.com/kb/1002310"
 $Display = "Table"
 $Author = "Rick Glover, Matthias Koehler, Dan Rowe"
-$PluginVersion = 1.5
+$PluginVersion = 1.6
 $PluginCategory = "vSphere"
 
 # Start of Settings
@@ -26,9 +26,9 @@ foreach ($eachDS in ($Datastores | Where-Object {$_.State -eq "Available"})) {
    Write-Progress -ID 2 -Parent 1 -Activity $pLang.pluginActivity -Status ($pLang.pluginStatus -f $i, $Datastores.count, $eachDS.Name) -PercentComplete ($i*100/$Datastores.count)
    
    $FilePath = $eachDS.DatastoreBrowserPath + '\*\*delta.vmdk*'
-   $fileList = @(Get-ChildItem -Path "$FilePath" | Select-Object Name, FolderPath, FullName)
+   $fileList = @(Get-ChildItem -Path "$FilePath" -ErrorAction SilentlyContinue | Select-Object Name, FolderPath, FullName)
    $FilePath = $eachDS.DatastoreBrowserPath + '\*\-*-flat.vmdk'
-   $fileList += Get-ChildItem -Path "$FilePath" | Select-Object Name, FolderPath, FullName
+   $fileList += Get-ChildItem -Path "$FilePath" -ErrorAction SilentlyContinue | Select-Object Name, FolderPath, FullName
 
    $i++
 
@@ -49,3 +49,7 @@ foreach ($eachDS in ($Datastores | Where-Object {$_.State -eq "Available"})) {
    }
 }
 Write-Progress -ID 1 -Activity $pLang.pluginActivity -Status $pLang.Complete -Completed
+
+# Changelog
+## 1.0 : Initial Version
+## 1.6 : Added the -ErrorAction SilentlyContinue parameter to the Get-ChildItem calls to prevent errors for files that no longer exist.
